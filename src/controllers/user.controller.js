2ccf229b5b6e0561a0de9292c83fd532e62bb933
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-05-21 00:11:26 +0500

    bug fixes, and register controller points completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,15 +8,17 @@ const registerUser = asyncHandler(async (req, res) => {
   // * Get user details from frontend
   // * Validation - not empty
   // * Check if user already exists: by username and email
-  //  Check for images, check for avatar
-  //  Upload avatar to cloudinary
-  //  Create user object - create entry in db
-  //  Remove password and refresh token fields from response
-  //  Check if the user is actually created
-  //  Return res
+  // * Check for images, check for avatar
+  // * Upload avatar to cloudinary
+  // * Create user object - create entry in db
+  // * Remove password and refresh token fields from response
+  // * Check if the user is actually created
+  // * Return res
 
   const { fullName, username, email, password } = req.body;
-  console.log("email: ", email);
+
+  // console.log("email: ", email);
+
   if (
     [fullName, email, username, password].some((field) => field?.trim() == "")
   ) {
@@ -35,8 +37,15 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) {
     throw new ApiError(400, "'avatar' file is required.");
   }
