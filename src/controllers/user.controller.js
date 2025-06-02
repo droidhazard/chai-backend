44f092150d875f7d14b6f09ea45d7c8943024d73
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-06-02 10:08:42 +0500

    logout user functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -117,7 +117,7 @@ const loginUser = asyncHandler(async (req, res) => {
     throw new ApiError(404, "user does not exist.");
   }
 
-  const isPasswordValid = await user.isPasswordCorrect();
+  const isPasswordValid = await user.isPasswordCorrect(password);
   if (!isPasswordValid) {
     throw new ApiError(401, "invalid user credentials.");
   }
@@ -152,6 +152,29 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-const logoutUser = asyncHandler(async (req, res) => {});
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
 
-export { registerUser, loginUser };
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully."));
+});
+
+export { registerUser, loginUser, logoutUser };
